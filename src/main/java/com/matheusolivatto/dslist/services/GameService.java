c41b9823c7585e9c3112abb6816be01168cdcf75
Matheus Franco Olivatto <mat.franco@hotmail.com>
2025-01-22 11:39:26 -0300

    GameDTO, search by id

--- src/main/java/com/matheusolivatto/dslist/services/GameService.java
+++ src/main/java/com/matheusolivatto/dslist/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.matheusolivatto.dslist.dto.GameDTO;
 import com.matheusolivatto.dslist.dto.GameMinDTO;
 import com.matheusolivatto.dslist.entities.Game;
 import com.matheusolivatto.dslist.repositories.GameRepository;
@@ -15,9 +17,15 @@ public class GameService {
 	@Autowired
 	public GameRepository gameRepository;
 	
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		return new GameDTO(result);
+	}
+	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll() {
 		List<Game> result = gameRepository.findAll();
 		return result.stream().map(x -> new GameMinDTO(x)).toList();
 	}
-	
 }
