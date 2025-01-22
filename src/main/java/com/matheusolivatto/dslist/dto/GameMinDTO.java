64de1829284069bd1ba31eb10dc6923ca055a4cd
Matheus Franco Olivatto <mat.franco@hotmail.com>
2025-01-22 12:29:20 -0300

    Projections

--- src/main/java/com/matheusolivatto/dslist/dto/GameMinDTO.java
+++ src/main/java/com/matheusolivatto/dslist/dto/GameMinDTO.java
@@ -1,6 +1,7 @@
 package com.matheusolivatto.dslist.dto;
 
 import com.matheusolivatto.dslist.entities.Game;
+import com.matheusolivatto.dslist.projections.GameMinProjection;
 
 public class GameMinDTO {
 
@@ -19,6 +20,14 @@ public class GameMinDTO {
 		year = entity.getYear();
 		imgUrl = entity.getImgUrl();
 		shortDescription = entity.getShortDescription();
+	}
+	
+	public GameMinDTO(GameMinProjection projection) {
+		id = projection.getId();
+		title = projection.getTitle();
+		year = projection.getYear();
+		imgUrl = projection.getImgUrl();
+		shortDescription = projection.getShortDescription();
 	}
 
 	public Long getId() {
@@ -40,7 +49,4 @@ public class GameMinDTO {
 	public String getShortDescription() {
 		return shortDescription;
 	}
-	
-	
-	
 }
